43dc4c94898e9098aeb84f64fa8f19d61f29564d
HussainAlAbdulkareem <hussain.alabdulkaree@ucalgary.ca>
2024-07-12 23:05:45 +0300

    Finds songs' URIs and adds them to a list.

--- main.py
+++ main.py
@@ -15,9 +15,13 @@ song_tags = soup.select(selector="li ul li h3")
 songs = [song_tag.getText().strip() for song_tag in song_tags]
 print(songs)
 
-
 auth = spotipy.oauth2.SpotifyOAuth(client_id=os.getenv("CLIENT_ID"), client_secret=os.getenv("CLIENT_SECRET"), redirect_uri="http://example.com")
 token = auth.get_access_token()["access_token"]
 
 sp = spotipy.Spotify(auth=token)
-user_name = sp.current_user()
+user_id = sp.current_user()["id"]
+
+songs_uris = []
+for song in songs:
+    song_uri = sp.search(q=f"track:{song} year:{date[:4]}/", type="track")
+    songs_uris.append(song_uri["tracks"]["items"][0]["uri"])
